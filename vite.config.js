f6b7af3ccded6595d182588117c1e4a891d05d89
Matt Thomas <matteverlove@gmail.com>
2026-02-06 15:08:19 -0500

    update base path to relative

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/color-font-generator/',
+  base: './', // Using relative path is safer for GitHub Pages
 })
